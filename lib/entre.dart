f911030a3a1c7c5c3b69649aeed0d21559be9d04
JOSE_CARLOS_BELTRAN_GAMEZ <a.21308051280319@cbtis128.edu.mx>
2024-04-10 16:12:40 +0000

    Actualizado

---
+++ lib/entre.dart
@@ -0,0 +1,45 @@
+import 'package:beltrandrawe2/drawer_menu.dart';
+import 'package:flutter/material.dart';
+
+class Entre extends StatelessWidget {
+  static const String routeName = '/entre';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Beltran Inicio",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Color(0xff034072),
+      ),
+      drawer: DrawerMenu(),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(0.0),
+            color: Colors.cyanAccent,
+            width: 80.0,
+            height: 80.0,
+          ),
+          Container(
+            padding: const EdgeInsets.all(0.0),
+            color: Colors.blueAccent,
+            width: 80.0,
+            height: 80.0,
+          ),
+          Container(
+            padding: const EdgeInsets.all(0.0),
+            color: Colors.orangeAccent,
+            width: 80.0,
+            height: 80.0,
+          ),
+        ],
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
